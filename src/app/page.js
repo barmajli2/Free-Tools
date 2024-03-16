728ce4b6c2506a7571068356917925656c3cafa2
Ahmed Mohamed <162905905+barmajli2@users.noreply.github.com>
2024-03-16 20:57:25 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 // import HeroSection from "./components/HeroSection";
 import SideBar from "./components/SideBar";
+import Linketer from './components/Linketer';
 
 export default function Home() {
   return (
     <main className="bg-gray-200">
       
       <SideBar/>
+      <Linketer/>
 
     </main>
   );
